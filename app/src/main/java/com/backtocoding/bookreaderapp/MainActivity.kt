2e39275facdfc967637d081a2d270ef37506e9cf
MilindAmrutkar <milind27.amrutkar@gmail.com>
2023-06-09 11:48:03 +0530

    DB testing

--- app/src/main/java/com/backtocoding/bookreaderapp/MainActivity.kt
+++ app/src/main/java/com/backtocoding/bookreaderapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.backtocoding.bookreaderapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,17 +12,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.backtocoding.bookreaderapp.ui.theme.BookReaderAppTheme
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BookReaderAppTheme {
+
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+                user["firstName"] = "Milind"
+                user["lastName"] = "Amrutkar"
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener {
+                            Log.d("FB", "onCreate: ${it.id}")
+                        }.addOnFailureListener {
+                            Log.d("FB", "onCreate: $it")
+                        }
                     Greeting("Android")
                 }
             }
