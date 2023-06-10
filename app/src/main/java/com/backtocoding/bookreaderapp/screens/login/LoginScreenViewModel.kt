8755848e84ed2cfd73ee5e8f44624867b369e4ee
MilindAmrutkar <milind27.amrutkar@gmail.com>
2023-06-10 08:34:52 +0530

    Added: Creation of users to Firebase Authentication

--- app/src/main/java/com/backtocoding/bookreaderapp/screens/login/LoginScreenViewModel.kt
+++ app/src/main/java/com/backtocoding/bookreaderapp/screens/login/LoginScreenViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 
@@ -31,7 +32,8 @@ class LoginScreenViewModel : ViewModel() {
                 auth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener { task ->
                         if (task.isSuccessful) {
-                            Log.d("FB", "signInWithEmailAndPassword: yayayaya ${task.result}")
+                            val displayName = task.result.user?.email?.split('@')?.get(0)
+                            createUser(displayName)
                             home()
                         } else {
                             Log.d("FB", "signInWithEmailAndPassword: ${task.result}")
@@ -41,6 +43,16 @@ class LoginScreenViewModel : ViewModel() {
                 Log.d("FB", "signInWithEmailAndPassword: ${ex.message}")
             }
         }
+
+    private fun createUser(displayName: String?) {
+        val userId = auth.currentUser?.uid
+        val user = mutableMapOf<String, Any>()
+        user["user_id"] = userId.toString()
+        user["display_name"] = displayName.toString()
+
+        FirebaseFirestore.getInstance().collection("users")
+            .add(user)
+    }
 
     fun createUserWithEmailAndPassword(email: String, password: String, home: () -> Unit) {
         if (_loading.value == false) {
